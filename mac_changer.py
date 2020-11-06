06d951a28c1207aebb3041ca9c5154de11f69460
jsfli <jeffrey.li@cantab.net>
2020-11-06 04:39:05 -0500

    changed subprocess

--- mac_changer.py
+++ mac_changer.py
@@ -5,9 +5,8 @@ import subprocess
 interface = input("interface > ")
 new_mac = input("new MAC > ")
 
-
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell = True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell = True)
-subprocess.call("ifconfig " + interface + " up", shell = True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
